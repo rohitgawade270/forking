792bedede447a4e64e90e94944f31f147cc5e38d
Rohan Patil <rp520671@gmail.com>
2023-04-10 12:53:51 +0530

    done

---
+++ react1/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import SignUp from './components/SignUp';
+
+
+function App() {
+  return (
+      <>
+      <div className="container">
+        <SignUp/>
+      </div>
+      
+      </>
+  );
+}
+
+export default App;
